e1316c3b19b18dee21cb4d6253475bca052eb3b7
CasperLCasper <64712311+CasperLCasper@users.noreply.github.com>
2026-07-05 13:59:56 +0300

    Update uploadMetadataToArweave.js

--- functions/api/uploadMetadataToArweave.js
+++ functions/api/uploadMetadataToArweave.js
@@ -94,10 +94,10 @@ export async function onRequestPost(context) {
       token: 'base-eth',
       gatewayUrl: 'https://sepolia.base.org',
       paymentServiceConfig: {
-        url: 'https://payment.ardrive.dev',
+        url: 'https://payment.ardrive.io',
       },
       uploadServiceConfig: {
-        url: 'https://upload.ardrive.dev',
+        url: 'https://upload.ardrive.io',
       }
     });
 
